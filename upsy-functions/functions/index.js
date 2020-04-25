37d471de6145e25d26f398bae09c7820b5c6baab
DumaAlexandru <alex.duma123@gmail.com>
2020-04-26 01:18:25 +0300

    Update

--- upsy-functions/functions/index.js
+++ upsy-functions/functions/index.js
@@ -1,8 +1,43 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+app.get('/tests', (req, res) =>{
+    admin.firestore()
+    .collection('tests')
+    .get()
+    .then(data => {
+        let tests = [];
+        data.forEach(doc=>{
+            tests.push(doc.data());
+        });
+        return res.json(tests);
+    })
+    .catch(err => console.error(err));
+})
+
+app.post('/test', (req, res) => {
+    const newTest = {
+        body: req.body.body,
+        submittedAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin.firestore()
+    .collection('tests')
+    .add(newTest)
+    .then(doc => {
+        res.json({message: `document ${doc.id} created succesfully`});
+    })
+    .catch(err => {
+        res.status(500).json({ error: "something went wrong"});
+        console.error(err);
+    })
+})
+
+
+// https://baseurl.com/api/blahblah
+exports.api = functions.region('europe-west3').https.onRequest(app);
